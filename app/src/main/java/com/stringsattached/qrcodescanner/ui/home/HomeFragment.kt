b3f0505f08976f582ff507a6327c172e86cc6238
kkrishna00 <krishnakishu9@gmail.com>
2023-08-29 20:53:05 +0530

    code : qr code scanner

--- app/src/main/java/com/stringsattached/qrcodescanner/ui/home/HomeFragment.kt
+++ app/src/main/java/com/stringsattached/qrcodescanner/ui/home/HomeFragment.kt
@@ -5,21 +5,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.gms.vision.CameraSource
-import com.stringsattached.qrcodescanner.R
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
 import com.stringsattached.qrcodescanner.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val requestCodeCameraPermission = 1001
-    private lateinit var cameraSource: CameraSource
+    private lateinit var codeScanner: CodeScanner
 
     private lateinit var viewModel: HomeViewModel
 
@@ -46,12 +48,42 @@ class HomeFragment : Fragment() {
         ) {
             askForCameraPermission()
         } else {
+            setupQRCodeScanner()
+        }
+    }
 
+    private fun setupQRCodeScanner() {
+        val scannerView = binding.scannerView
+
+        codeScanner = CodeScanner(requireContext(), scannerView)
+
+        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
+        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
+        // ex. listOf(BarcodeFormat.QR_CODE)
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+        codeScanner.scanMode = ScanMode.CONTINUOUS // or CONTINUOUS or PREVIEW
+        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
+        codeScanner.isFlashEnabled = false // Whether to enable flash or not
+
+        // Callbacks
+        codeScanner.decodeCallback = DecodeCallback {
+            activity?.runOnUiThread {
+                Toast.makeText(requireContext(), "Scan result: ${it.text}", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
+            activity?.runOnUiThread {
+                Toast.makeText(
+                    requireContext(), "Camera initialization error: ${it.message}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
 
-        val aniSlide: Animation =
-            AnimationUtils.loadAnimation(requireContext(), R.anim.scanner_animation)
-        binding.barcodeLine.startAnimation(aniSlide)
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
     }
 
     private fun askForCameraPermission() {
@@ -62,9 +94,18 @@ class HomeFragment : Fragment() {
         )
     }
 
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
-        cameraSource.stop()
         _binding = null
     }
 }
